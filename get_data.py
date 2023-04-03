2c3b9a2a7026921bc19f4749aa369fb173188a89
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2023-04-03 18:17:26 +0100

    added comments and basic data clean function

--- get_data.py
+++ get_data.py
@@ -3,22 +3,35 @@ import snscrape.modules.twitter as sntwitter
 
 class DataProcessor():
 
+    # initialise variables
     def __init__(self, user_list, since_date, until_date):
         self.user_list = user_list
         self.since_date = since_date 
         self.until_date = until_date 
 
+    # function to scrape twitter for tweets
     def scrape_twitter(self):
-        # Set variables for scraping
-        # Scrape tweets
+
+        # array to store tweets
         tweets_list = []
+
+        # get tweets for each user
         for user in self.user_list:
             count = 0
+
+            # get tweets for user
             for tweet in sntwitter.TwitterSearchScraper(f'from:{user} since:{self.since_date} until:{self.until_date}').get_items():
                 count = count + 1
+
+                # add tweet to tweets_list array
                 tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.user.followersCount, tweet.retweetCount, tweet.likeCount])
                 if count == 10:
                     break
 
-        # Create pandas dataframe
+        #return dataframe
         return pd.DataFrame(tweets_list, columns=['date', 'id', 'text', 'username', 'followers', 'retweets', 'likes'])
+    
+    def clean_data(self, data):
+        #cleandata
+        clean_data = data
+        return clean_data
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from get_data import DataProcessor
 
+# variables for scraping tweets
 user_list = ['elonmusk', 'katyperry', 'ksi']
 since_date = '2022-01-01'
 until_date = '2022-03-31'
+
+# initialise DataProcessor class
 data_class = DataProcessor(user_list, since_date, until_date)
 
-
+# scrape twitter for tweets
 twitter_data = data_class.scrape_twitter()
 
-print(twitter_data)
+# clear scraped data
+clean_twitter_date = data_class.clean_data(twitter_data)
+
+# print data
+print(clean_twitter_date)
