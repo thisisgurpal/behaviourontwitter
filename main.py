3c80052be9334e45f23ed70a4966f4c1c04743de
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2023-04-03 17:27:26 +0100

    added twitter scrape script

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import snscrape.modules.twitter as sntwitter
 
-query = "sarcasm (#sarcasm) lang:en since:2023-03-01"
-tweets = []
-limit = 100
+# Set variables for scraping
+user_list = ['elonmusk', 'katyperry', 'ksi'] #examples
+since_date = '2022-01-01'
+until_date = '2022-03-31'
+# Scrape tweets
+tweets_list = []
+for user in user_list:
+    count = 0
+    for tweet in sntwitter.TwitterSearchScraper(f'from:{user} since:{since_date} until:{until_date}').get_items():
+        count = count + 1
+        tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.user.followersCount, tweet.retweetCount, tweet.likeCount])
+        if count == 10:
+            break
 
-for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-    
-    if len(tweets) == limit:
-        break
-    else:
-        tweets.append([tweet.date, tweet.user.username, tweet.rawContent])
+# Create pandas dataframe
+tweets_df = pd.DataFrame(tweets_list, columns=['date', 'id', 'text', 'username', 'followers', 'retweets', 'likes'])
 
-df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
-
-print(df)
